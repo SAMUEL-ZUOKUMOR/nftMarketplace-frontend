fbbb03bca0f7fa8d79a50f87d2b740bb431e214c
SAMUEL ZUOKUMOR <99648977+SAMUEL-ZUOKUMOR@users.noreply.github.com>
2022-10-26 16:19:16 +0100

    first commit

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/samuel-zuokumor/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xFfe1421Ff3839aD79297A1B4b1294B298e4A7B36"; 
